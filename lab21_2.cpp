02cb432ab76fec24a85ecdb9cab9f290c2bfe420
auswitch <sawit_cha@cmu.ac.th>
2022-01-30 13:50:29 +0700

    lab21.1 and lab21.2 completed

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
-}
+double overlap(Rect *R1, Rect *R2)
+{
+    double w = 0, h = 0;
+	if(R1->w + R1->x > R2->x)
+	{
+		if(R1->x + R1->w < R2->x + R2->w)
+			w = R1->w - abs(R2->x - R1->x);
+		else
+			w = R2->w;
+		if(R1->y + R1->h < R2->y + R2->h)
+			h = R2->h - abs(R2->y - R1->y);
+		else
+			h = R2->h;
+	}
+	return w*h; 
